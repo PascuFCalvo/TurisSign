726da3eb5d2ebc9e5c2f658c5ca97eb9abf37a12
Pascual Fernandez Calvo <pascual.fernandez.calvo@gmail.com>
2024-11-13 09:20:41 +0100

    first

--- proxy.js
+++ proxy.js
@@ -4,6 +4,12 @@ import cors from "cors";
 import fetch from "node-fetch";
 import multer from "multer";
 import nodemailer from "nodemailer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 app.use(cors());
